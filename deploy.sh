d840e75d695d84aa8e49b781e06a146dfdb6f067
Bolivar26 <cesarbolivar.bt@gmail.com>
2021-08-24 22:49:08 -0400

    github pages

--- deploy.sh
+++ deploy.sh
@@ -10,5 +10,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-git push -f git@github.com:cesarterrero/Prueba_Corotos.git master:gh-pages
+git push -f git@github.com:cesarterrero/Prueba_Corotos.git main:gh-pages
 cd -
